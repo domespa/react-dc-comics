eb6db7d31aaa586215f5530af0fca7dd1501e376
domespa <dumiii1988@gmail.com>
2025-01-28 19:23:08 +0100

    add footer bottom

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,29 +3,151 @@ export default function Footer() {
     <footer>
       <div className="footer-top">
         <div className="container footer-icons">
-          <div>
+          <div className="cont-footop">
             <img src="img/buy-comics-digital-comics.png" alt="Digital" />
             <span>DIGITAL COMICS</span>
           </div>
-          <div>
+          <div className="cont-footop">
             <img src="img/buy-comics-merchandise.png" alt="Merchandise" />
             <span>DC MERCHANDISE</span>
           </div>
-          <div>
+          <div className="cont-footop">
             <img src="img/buy-comics-subscriptions.png" alt="Subscriptions" />
             <span>SUBSCRIPTION</span>
           </div>
-          <div>
+          <div className="cont-footop">
             <img src="img/buy-comics-shop-locator.png" alt="Shop" />
             <span>COMIC SHOP LOCATOR</span>
           </div>
-          <div>
+          <div className="cont-footop">
             <img src="img/buy-dc-power-visa.svg" alt="Visa" />
             <span>DC POWER VISA</span>
           </div>
         </div>
       </div>
-      <div></div>
+      <div className="footer-center">
+        <div className="container">
+          <div className="list-foot">
+            <div>
+              <h4>DC COMICS</h4>
+              <ul>
+                <li>
+                  <a href="#">Characters</a>
+                </li>
+                <li>
+                  <a href="#">Comics</a>
+                </li>
+                <li>
+                  <a href="#">Movies</a>
+                </li>
+                <li>
+                  <a href="#">TV</a>
+                </li>
+                <li>
+                  <a href="#">Games</a>
+                </li>
+                <li>
+                  <a href="#">Videos</a>
+                </li>
+                <li>
+                  <a href="#">News</a>
+                </li>
+              </ul>
+              <h4>SHOP</h4>
+              <ul>
+                <li>
+                  <a href="#">Shop DC</a>
+                </li>
+                <li>
+                  <a href="#">Shop DC Collectibles</a>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <h4>DC</h4>
+              <ul>
+                <li>
+                  <a href="#">Terms Of Use</a>
+                </li>
+                <li>
+                  <a href="#">Privacy policy (New)</a>
+                </li>
+                <li>
+                  <a href="#">Ad Choices</a>
+                </li>
+                <li>
+                  <a href="#">Advertising</a>
+                </li>
+                <li>
+                  <a href="#">Jobs</a>
+                </li>
+                <li>
+                  <a href="#">Subscriptions</a>
+                </li>
+                <li>
+                  <a href="#">Talent Workshops</a>
+                </li>
+                <li>
+                  <a href="#">CPSC Certificates</a>
+                </li>
+                <li>
+                  <a href="#">Ratings</a>
+                </li>
+                <li>
+                  <a href="#">Shop Help</a>
+                </li>
+                <li>
+                  <a href="#">Contact US</a>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <h4>SITES</h4>
+              <ul>
+                <li>
+                  <a href="#">DC</a>
+                </li>
+                <li>
+                  <a href="#">MAD Magazine</a>
+                </li>
+                <li>
+                  <a href="#">DC Kids</a>
+                </li>
+                <li>
+                  <a href="#">DC Universe</a>
+                </li>
+                <li>
+                  <a href="#">DC Power Visa</a>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <img
+                className="logo-foot"
+                src="img/dc-logo-bg.png"
+                alt="DC Logo"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="footer-bot">
+        <div className="container">
+          <div className="cont-foot-bot">
+            <button>SIGN-UP NOW</button>
+            <div className="social-button">
+              <span>
+                <a href="#">FOLLOW US</a>
+              </span>
+              <img src="img/footer-facebook.png" alt="Facebook" />
+              <img src="img/footer-twitter.png" alt="Twitter" />
+              <img src="img/footer-youtube.png" alt="YouTube" />
+              <img src="img/footer-pinterest.png" alt="Pinterest" />
+              <img src="img/footer-periscope.png" alt="Periscope" />
+            </div>
+          </div>
+        </div>
+      </div>
     </footer>
   );
 }
