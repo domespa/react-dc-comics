1802f02cccf8b35f06d0819d6562470c7bd35300
domespa <dumiii1988@gmail.com>
2025-01-28 16:49:09 +0100

    add footer top

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,31 @@
 export default function Footer() {
-  return <footer>ee</footer>;
+  return (
+    <footer>
+      <div className="footer-top">
+        <div className="container footer-icons">
+          <div>
+            <img src="img/buy-comics-digital-comics.png" alt="Digital" />
+            <span>DIGITAL COMICS</span>
+          </div>
+          <div>
+            <img src="img/buy-comics-merchandise.png" alt="Merchandise" />
+            <span>DC MERCHANDISE</span>
+          </div>
+          <div>
+            <img src="img/buy-comics-subscriptions.png" alt="Subscriptions" />
+            <span>SUBSCRIPTION</span>
+          </div>
+          <div>
+            <img src="img/buy-comics-shop-locator.png" alt="Shop" />
+            <span>COMIC SHOP LOCATOR</span>
+          </div>
+          <div>
+            <img src="img/buy-dc-power-visa.svg" alt="Visa" />
+            <span>DC POWER VISA</span>
+          </div>
+        </div>
+      </div>
+      <div></div>
+    </footer>
+  );
 }
