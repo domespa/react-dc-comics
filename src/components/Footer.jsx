1def09caa1463bafa7eb3fe4b80177dd8ee260c8
domespa <dumiii1988@gmail.com>
2025-01-29 15:56:19 +0100

    add bonus

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,105 +1,27 @@
+import footerLinks from "../data/footerLinks";
+
 export default function Footer() {
   return (
     <footer>
       <div className="footer-center">
         <div className="container">
           <div className="list-foot">
-            <div>
-              <h4>DC COMICS</h4>
-              <ul>
-                <li>
-                  <a href="#">Characters</a>
-                </li>
-                <li>
-                  <a href="#">Comics</a>
-                </li>
-                <li>
-                  <a href="#">Movies</a>
-                </li>
-                <li>
-                  <a href="#">TV</a>
-                </li>
-                <li>
-                  <a href="#">Games</a>
-                </li>
-                <li>
-                  <a href="#">Videos</a>
-                </li>
-                <li>
-                  <a href="#">News</a>
-                </li>
-              </ul>
-              <h4>SHOP</h4>
-              <ul>
-                <li>
-                  <a href="#">Shop DC</a>
-                </li>
-                <li>
-                  <a href="#">Shop DC Collectibles</a>
-                </li>
-              </ul>
-            </div>
-            <div>
-              <h4>DC</h4>
-              <ul>
-                <li>
-                  <a href="#">Terms Of Use</a>
-                </li>
-                <li>
-                  <a href="#">Privacy policy (New)</a>
-                </li>
-                <li>
-                  <a href="#">Ad Choices</a>
-                </li>
-                <li>
-                  <a href="#">Advertising</a>
-                </li>
-                <li>
-                  <a href="#">Jobs</a>
-                </li>
-                <li>
-                  <a href="#">Subscriptions</a>
-                </li>
-                <li>
-                  <a href="#">Talent Workshops</a>
-                </li>
-                <li>
-                  <a href="#">CPSC Certificates</a>
-                </li>
-                <li>
-                  <a href="#">Ratings</a>
-                </li>
-                <li>
-                  <a href="#">Shop Help</a>
-                </li>
-                <li>
-                  <a href="#">Contact US</a>
-                </li>
-              </ul>
-            </div>
-            <div>
-              <h4>SITES</h4>
-              <ul>
-                <li>
-                  <a href="#">DC</a>
-                </li>
-                <li>
-                  <a href="#">MAD Magazine</a>
-                </li>
-                <li>
-                  <a href="#">DC Kids</a>
-                </li>
-                <li>
-                  <a href="#">DC Universe</a>
-                </li>
-                <li>
-                  <a href="#">DC Power Visa</a>
-                </li>
-              </ul>
-            </div>
+            {footerLinks.map((footerLink) => (
+              <div key={footerLink.id}>
+                <h4>{footerLink.title}</h4>
+                <ul>
+                  {footerLink.links.map((link) => (
+                    <li key={link.id}>
+                      <a href={link.url}>{link.text}</a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ))}
             <div>
               <img
                 className="logo-foot"
+                s
                 src="img/dc-logo-bg.png"
                 alt="DC Logo"
               />
---
+++ src/data/footerLinks.js
@@ -0,0 +1,50 @@
+export default [
+  {
+    id: 1,
+    title: "DC COMICS",
+    links: [
+      { id: 1, url: "#", text: "Characters" },
+      { id: 2, url: "#", text: "Comics" },
+      { id: 3, url: "#", text: "Movies" },
+      { id: 4, url: "#", text: "TV" },
+      { id: 5, url: "#", text: "Games" },
+      { id: 6, url: "#", text: "Videos" },
+      { id: 7, url: "#", text: "News" },
+    ],
+  },
+  {
+    id: 2,
+    title: "SHOP",
+    links: [
+      { id: 1, url: "#", text: "Shop DC" },
+      { id: 2, url: "#", text: "Shop DC Collectibles" },
+    ],
+  },
+  {
+    id: 3,
+    title: "DC",
+    links: [
+      { id: 1, url: "#", text: "Terms Of Use" },
+      { id: 2, url: "#", text: "Privacy policy (New)" },
+      { id: 3, url: "#", text: "Ad Choices" },
+      { id: 4, url: "#", text: "Advertising" },
+      { id: 5, url: "#", text: "Jobs" },
+      { id: 6, url: "#", text: "Subscriptions" },
+      { id: 7, url: "#", text: "Talent Workshops" },
+      { id: 8, url: "#", text: "Ratings" },
+      { id: 9, url: "#", text: "Shop Help" },
+      { id: 10, url: "#", text: "Contact US" },
+    ],
+  },
+  {
+    id: 4,
+    title: "SITES",
+    links: [
+      { id: 1, url: "#", text: "DC" },
+      { id: 2, url: "#", text: "MAD Magazine" },
+      { id: 3, url: "#", text: "DC Kids" },
+      { id: 4, url: "#", text: "DC Universe" },
+      { id: 5, url: "#", text: "DC Power Visa" },
+    ],
+  },
+];
