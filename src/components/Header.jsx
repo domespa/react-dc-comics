aaf0c1eced06cfb0e0b6a8f6ed0c66a683313956
domespa <dumiii1988@gmail.com>
2025-01-29 15:11:22 +0100

    add function for headerNav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,39 +1,18 @@
+import headerMenus from "../data/headerMenus";
+
 export default function Header() {
   return (
     <header className="container">
       <img src="img/dc-logo.png" alt="Logo DC" />
       <div className="nav-header">
         <ul>
-          <li>
-            <a href="#">CHARACTERS</a>
-          </li>
-          <li>
-            <a href="#">COMICS</a>
-          </li>
-          <li>
-            <a href="#">MOVIES</a>
-          </li>
-          <li>
-            <a href="#">TV</a>
-          </li>
-          <li>
-            <a href="#">GAMES</a>
-          </li>
-          <li>
-            <a href="#">COLLECTIBLES</a>
-          </li>
-          <li>
-            <a href="#">VIDEOS</a>
-          </li>
-          <li>
-            <a href="#">FANS</a>
-          </li>
-          <li>
-            <a href="#">NEWS</a>
-          </li>
-          <li>
-            <a href="#">SHOP</a>
-          </li>
+          {headerMenus.map((headerNav) => {
+            return (
+              <li key={headerNav.id}>
+                <a href={headerNav.url}>{headerNav.title}</a>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </header>
---
+++ src/data/headerMenus.js
@@ -0,0 +1,52 @@
+export default [
+  {
+    id: 1,
+    title: "CHARACTERS",
+    url: "#",
+  },
+  {
+    id: 2,
+    title: "COMICS",
+    url: "#",
+  },
+  {
+    id: 3,
+    title: "MOVIES",
+    url: "#",
+  },
+  {
+    id: 4,
+    title: "TV",
+    url: "#",
+  },
+  {
+    id: 5,
+    title: "GAMES",
+    url: "#",
+  },
+  {
+    id: 6,
+    title: "COLLECTIBLES",
+    url: "#",
+  },
+  {
+    id: 7,
+    title: "VIDEOS",
+    url: "#",
+  },
+  {
+    id: 8,
+    title: "FANS",
+    url: "#",
+  },
+  {
+    id: 9,
+    title: "NEWS",
+    url: "#",
+  },
+  {
+    id: 10,
+    title: "SHOP",
+    url: "#",
+  },
+];
